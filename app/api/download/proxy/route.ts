1aebcac1a51427268582dca6e07142a1a44b581e
CJ <cjfritz9@gmail.com>
2024-03-12 16:41:28 -0400

    trying new string replacement for periods in ig usernames to url

--- app/api/download/proxy/route.ts
+++ app/api/download/proxy/route.ts
@@ -3,29 +3,22 @@ import { APIResponse } from '../../utils';
 import { igClient } from '../../clients';
 
 export const GET = async (req: NextRequest) => {
-  try {
-    const origin = req.nextUrl.searchParams.get('origin');
-    if (origin) {
-      const formattedOrigin = origin.slice(origin.indexOf('/?url=') + 6);
-      const mediaResponse = await fetch(formattedOrigin, {
-        cache: 'no-cache',
-        headers: {
-          'Content-Type': 'application/octet-stream',
-        },
-      });
+  const origin = req.nextUrl.searchParams.get('origin');
+  console.log(origin);
+  if (origin) {
+    const formattedOrigin = origin.slice(origin.indexOf('/?url=') + 6);
+    const mediaResponse = await fetch(formattedOrigin, {
+      cache: 'no-cache',
+    });
 
-      if (mediaResponse && mediaResponse.ok) {
-        return mediaResponse;
-      } else {
-        console.log(await mediaResponse?.text());
-      }
+    if (mediaResponse && mediaResponse.ok) {
+      return mediaResponse;
+    } else {
+      console.log(await mediaResponse?.text());
     }
-
-    return NextResponse.json(new APIResponse('ok', null, null));
-  } catch (error) {
+  } else {
     return NextResponse.json(
       new APIResponse('error', 'Internal server error', null)
     );
-    console.error(error);
   }
 };
