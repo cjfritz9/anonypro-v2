6dcf37a48a8edae5bf664c3cdce82c34c8465c9d
CJ <cjfritz9@gmail.com>
2024-05-07 11:42:04 -0400

    revert from Next Script to html script for testing. ads are no longer showing in prod even after reverting to the last commit that they were showing

--- app/[locale]/template.tsx
+++ app/[locale]/template.tsx
@@ -3,11 +3,11 @@ import Script from 'next/script';
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Script
+      <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9924000383649266"
         crossOrigin="anonymous"
-      ></Script>
+      ></script>
       {children}
     </>
   );
