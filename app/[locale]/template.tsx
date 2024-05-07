19ae794597bc5241d799b1b68562a46106213815
CJ <cjfritz9@gmail.com>
2024-05-07 10:42:54 -0400

    fix for staging, remove Next Script strategy to debug why ads are now not showing when there are no issues

--- app/[locale]/template.tsx
+++ app/[locale]/template.tsx
@@ -5,7 +5,6 @@ export default function Template({ children }: { children: React.ReactNode }) {
     <>
       <Script
         async
-        strategy="beforeInteractive"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9924000383649266"
         crossOrigin="anonymous"
       />
