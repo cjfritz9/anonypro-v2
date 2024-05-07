6dd6e96b7bac9c442e722b16ca881d49e3b98ed6
CJ Fritz <110115371+cjfritz9@users.noreply.github.com>
2024-05-07 13:56:51 -0400

    Update DisplayAd.tsx

--- components/Ads/DisplayAd.tsx
+++ components/Ads/DisplayAd.tsx
@@ -15,14 +15,16 @@ const DisplayAd: React.FC = () => {
   if (!isClient) return null;
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block' }}
-      data-ad-client="ca-pub-9924000383649266"
-      data-ad-slot="7391767564"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
+    <div>
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-9924000383649266"
+        data-ad-slot="7391767564"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
   );
 };
 
